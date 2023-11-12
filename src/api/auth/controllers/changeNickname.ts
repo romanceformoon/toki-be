28691893aae4f3ace0347d5d46ed7e4b4aabeeab
romanceformoon <lmwljw98@gmail.com>
2023-11-13 02:33:36 +0900

    feat: change nickname added

--- src/api/auth/controllers/changeNickname.ts
+++ src/api/auth/controllers/changeNickname.ts
@@ -3,28 +3,31 @@ import jwt from 'jsonwebtoken';
 import { IAuth } from '~/@types/auth';
 import { logger } from '~/config/winston';
 
-export const checkUser = async (req: Request, res: Response) => {
+export const changeNickname = async (req: Request, res: Response) => {
     try {
         if (!req.accessToken) return res.status(401).send('Not authorized');
+
+        const nickname = req.params.nickname;
+
+        if (nickname.length > 16 || nickname.length === 0)
+            return res.status(400).json({ result: 'Wrong input' });
 
         const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as jwt.Secret;
 
         jwt.verify(req.accessToken, JWT_SECRET_KEY);
         const decoded = jwt.decode(req.accessToken) as IAuth;
 
-        const [userQuery] = await req.database.query(
-            'SELECT nickname, avatar FROM user WHERE uid = ?',
-            [decoded['uid']]
-        );
-
-        req.database.end();
+        try {
+            await req.database.query(
+                'UPDATE user SET nickname = ? WHERE uid = ?',
+                [nickname, decoded['uid']]
+            );
+        } catch (err) {
+            return res.status(500).json({ result: 'DB Failed' });
+        }
 
         return res.status(200).json({
-            user: {
-                uid: decoded['uid'],
-                nickname: userQuery[0].nickname,
-                avatar: userQuery[0].avatar,
-            },
+            nickname,
         });
     } catch (err) {
         logger.error(err);
--- src/api/auth/routes/user.ts
+++ src/api/auth/routes/user.ts
@@ -1,13 +1,21 @@
 import express, { Router } from 'express';
 import { refresh } from '~/api/auth/controllers/refresh';
 import { authChecker } from '~/middlewares/authChecker';
+import { databaseConnector } from '~/middlewares/databaseConnector';
+import { changeNickname } from '../controllers/changeNickname';
 import { checkUser } from '../controllers/checkUser';
 import { logout } from '../controllers/logout';
 
 const router: Router = express.Router();
 
 router.get('/refresh', refresh);
-router.get('/check-user', authChecker, checkUser);
+router.get('/check-user', authChecker, databaseConnector, checkUser);
 router.get('/logout', authChecker, logout);
+router.post(
+    '/change-nickname/:nickname',
+    authChecker,
+    databaseConnector,
+    changeNickname
+);
 
 module.exports = router;
