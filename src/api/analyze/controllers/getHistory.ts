173c6c5ec1fafb5163bff3fe8e939b916e50c354
romanceformoon <lmwljw98@gmail.com>
2023-11-26 01:25:53 +0900

    fix: graph, history return type changed

--- src/api/analyze/controllers/getHistory.ts
+++ src/api/analyze/controllers/getHistory.ts
@@ -19,12 +19,29 @@ export const getHistory = async (req: Request, res: Response) => {
 
             db.close();
 
-            return res.status(200).json({
-                history,
-            });
+            return res.status(200).json(history);
         } else {
             return res.status(200).json({
-                result: 'Failed',
+                'LEVEL 1': [],
+                'LEVEL 2': [],
+                'LEVEL 3': [],
+                'LEVEL 4': [],
+                'LEVEL 5': [],
+                'LEVEL 6': [],
+                'LEVEL 7': [],
+                'LEVEL 8': [],
+                'LEVEL 9': [],
+                'LEVEL 10': [],
+                'LEVEL 11': [],
+                'LEVEL 12': [],
+                'LEVEL 13': [],
+                'LEVEL 14': [],
+                'LEVEL 15': [],
+                'LEVEL 16': [],
+                'LEVEL 17': [],
+                'LEVEL 18': [],
+                'LEVEL 19': [],
+                'LEVEL 20': [],
             });
         }
     } catch (err) {
