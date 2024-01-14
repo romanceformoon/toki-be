e094ed3fa87309c2bb799258c33ba601e336828b
romanceformoon <lmwljw98@gmail.com>
2024-01-15 01:49:53 +0900

    chore: aery rating data updated

--- crawl.py
+++ crawl.py
@@ -3,9 +3,9 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-# _resp = requests.get('https://asumatoki.kr/table/aery/data.json')
+_resp = requests.get('https://asumatoki.kr/table/aery/data.json')
 # _resp = requests.get('https://stellabms.xyz/sl/score.json')
-_resp = requests.get('https://stellabms.xyz/st/score.json')
+# _resp = requests.get('https://stellabms.xyz/st/score.json')
 aery_data = _resp.json()
 
 data = {}
@@ -17,18 +17,23 @@ for _ in aery_data:
         soup = BeautifulSoup(html, 'html.parser')
         result = soup.select("table:nth-of-type(3) > tr:nth-of-type(4) > td")
 
-        fc_ratio = result[0].text
-        hard_ratio = result[1].text
-        groove_ratio = result[2].text
-        easy_ratio = result[3].text
+        try:
+            fc_ratio = result[0].text
+            hard_ratio = result[1].text
+            groove_ratio = result[2].text
+            easy_ratio = result[3].text
 
-        print({
-            "fc_ratio": fc_ratio, "hard_ratio": hard_ratio, "groove_ratio": groove_ratio, "easy_ratio": easy_ratio
-        })
+            print({
+                "fc_ratio": fc_ratio, "hard_ratio": hard_ratio, "groove_ratio": groove_ratio, "easy_ratio": easy_ratio, "level": _["level"], "title": _["title"]
+            })
 
-        data[_["md5"]] = {
-            "fc_ratio": fc_ratio, "hard_ratio": hard_ratio, "groove_ratio": groove_ratio, "easy_ratio": easy_ratio
-        }
+            data[_["md5"]] = {
+                "fc_ratio": fc_ratio, "hard_ratio": hard_ratio, "groove_ratio": groove_ratio, "easy_ratio": easy_ratio, "level": _["level"], "title": _["title"]
+            }
+        except:
+            data[_["md5"]] = {
+                "fc_ratio": '0%', "hard_ratio": '0%', "groove_ratio": '0%', "easy_ratio": '0%', "level": _["level"], "title": _["title"]
+            }
 
 
 file_path = "./ratingData.json"
